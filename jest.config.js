45efcaea574c04159d05a0235810c49caac3f01e
Nafih Yalım <nafihyalim@fiyuu.com.tr>
2026-06-08 11:27:01 +0300

    jest config export test results

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
+const reporters = process.env.CI
+  ? [
+      'default',
+      [
+        'jest-junit',
+        {
+          outputDirectory: 'test-results',
+          outputName: 'junit.xml',
+        },
+      ],
+    ]
+  : ['default'];
+
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
@@ -5,4 +18,5 @@ module.exports = {
     'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-gesture-handler|react-native-reanimated|react-native-screens|react-native-safe-area-context|react-native-iphone-x-helper|@react-native-community|lottie-react-native)/)',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  reporters,
 };
